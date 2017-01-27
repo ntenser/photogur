a0acb812cf7254bbc17531ec27233c5ef58cdb31
noah tenser <ntenser@hotmail.com>
2017-01-27 13:35:06 -0500

    Creating Picture model in database

---
+++ app/models/picture.rb
@@ -0,0 +1,14 @@
+class Picture < ActiveRecord::Base
+end
+
+class CreatePictures < ActiveRecord::Migration
+  def change
+    create_table :pictures do |t|
+      t.string :artist
+      t.string :title
+      t.string :url
+
+      t.timestamps null: false
+    end
+  end
+end
